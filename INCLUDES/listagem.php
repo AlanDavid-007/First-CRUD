787932ed60ea36f5bf7a1abd3830eaf6bc69a939
AlanDavid-007 <alanpereiradavid@gmail.com>
2022-03-14 22:00:14 -0300

    1

---
+++ INCLUDES/listagem.php
@@ -0,0 +1,30 @@
+<section>
+    <table class="table bg-light mt-3">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Descrição</th>
+                <th>Data</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>Teste título 1</td>
+            <td>Descrição teste 1</td>
+            <td>11/11/2011 11:11:11</td>
+            <td>Ativo</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Teste título 2</td>
+            <td>Descrição teste 2</td>
+            <td>22/22/2022 22:22:22</td>
+            <td>Inativo</td>
+        </tr>
+        </tbody>
+    <table>
+</section>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+    
+    require __DIR__.'/INCLUDES/header.php';
+
+    require __DIR__.'/INCLUDES/listagem.php';
+
+    require __DIR__.'/INCLUDES/footer.php';
+        
+?>
